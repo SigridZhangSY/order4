7dca224ec542684b04804febfc5b37e5b5df650a
SigridZhangSY <sigridzhang0603@163.com>
2016-07-18 11:15:00 +0800

    init

--- src/main/java/com/thoughtworks/ketsu/web/jersey/Routes.java
+++ src/main/java/com/thoughtworks/ketsu/web/jersey/Routes.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.ketsu.web.jersey;
 
-import com.thoughtworks.ketsu.domain.user.User;
+import com.thoughtworks.ketsu.infrastructure.core.Product;
 
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
@@ -9,11 +9,14 @@ public class Routes {
 
     private final String baseUri;
 
-    public Routes(UriInfo uriInfo) {
-        baseUri = uriInfo.getBaseUri().toASCIIString();
+//    public Routes(UriInfo uriInfo) {
+//        baseUri = uriInfo.getBaseUri().toASCIIString();
+//    }
+
+    public Routes() {
+        baseUri = "/";
     }
 
-    public URI userUrl(User user) {
-        return URI.create(String.format("%susers/%s", baseUri, user.getUserId().id()));
-    }
+
+
 }
